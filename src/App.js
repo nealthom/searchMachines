d90b1f903a44c3f22c5f5b4fa2045fa717469200
nealthom <nealthom@gmail.com>
2020-01-04 06:06:42 -0600

    Working on Filter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ReactFileReader from "react-file-reader";
 import Papa from "papaparse";
 
 import CardList from "./components/card-list/card-list.component";
-import SearchBox from './components/search-box/search-box.component'
+import SearchBox from "./components/search-box/search-box.component";
 
 import useLocalStorage from "./effects/use-localStorage.effect";
 
@@ -11,22 +11,20 @@ import "./App.css";
 
 const App = () => {
   const [games, setGames] = useLocalStorage("games", []);
-  const [searchField, setSearchField] = useState('search')
-  const [filteredGames, setFilteredGames] = useState([])
+  const [searchField, setSearchField] = useState("search");
+  const [filteredGames, setFilteredGames] = useState([]);
 
-  useEffect(()=>{
-    console.log(searchField)
-const newGames = games.filter(
-    game =>{
-      if(game['Long Name'])
-        return game
-    //return game["Long Name"].toLowerCase().includes(searchField.toLowerCase()))
-    })
-  setFilteredGames(newGames.filter(game=> {
-    game['Long Name'].toLowerCase().includes(searchField.toLowerCase())
-  }))
-  
-  },[searchField,games])
+  useEffect(() => {
+    const newGames = games.filter(game => game.hasOwnProperty("Long Name"));
+
+    const removedDupes = [...new Set(newGames)];
+
+    setFilteredGames(
+      removedDupes.filter(game =>
+        game["Long Name"].toLowerCase().includes(searchField.toLowerCase())
+      )
+    );
+  }, [searchField, games]);
 
   const handleFiles = files => {
     let reader = new FileReader();
@@ -37,17 +35,14 @@ const newGames = games.filter(
     reader.readAsText(files[0]);
   };
 
-  const onSearchChange = event =>{
-    setSearchField(event.target.value)
-    
+  const onSearchChange = event => {
+    setSearchField(event.target.value);
+  };
 
-  }
-
-  
   return (
     <div className="App">
-      <h1>Machine Search</h1>
-      <SearchBox onSearchChange={onSearchChange}/>
+      <h1>Search</h1>
+      <SearchBox onSearchChange={onSearchChange} />
       <ReactFileReader handleFiles={handleFiles} fileTypes={".csv"}>
         <button>Upload New List</button>
       </ReactFileReader>
